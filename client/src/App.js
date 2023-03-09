00b55ae46249bc6b321528a151fed59e3bd543ae
Rohit Dhakane <rohitdhakane6@gmail.com>
2023-03-09 15:51:05 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/HomePage"
+import LoginPage from "./scenes/loginPage"
+// import ProfilePage from "./scenes/";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Navbar from "./scenes/navbar";
+
+
+function App() {
+  
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              // element={isAuth ? <HomePage />:<Navigate to="/" /> }
+              element={<Navbar/>}
+            />
+             {/* <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            /> */}
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ client/src/scenes/HomePage/index.jsx
@@ -0,0 +1,6 @@
+import Navbar from "../navbar"
+
+const HomePage = () => {
+    <Navbar/>
+}
+export default HomePage;
